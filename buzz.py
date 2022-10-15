142a593299919cf740ea63568acbe387e642923c
ErrorDecoder <ErrorDecoder@github.com>
2022-10-15 19:31:29 +0530

    Oct15

--- buzz.py
+++ buzz.py
@@ -5,47 +5,42 @@ from time import sleep
 
 # def buzz():
 
+while True:
+    try:
+        GPIO.setmode(GPIO.BOARD)
 
-try:
-    GPIO.setmode(GPIO.BOARD)
+        PIN_TRIGGER = 7
+        PIN_ECHO = 11
 
-    PIN_TRIGGER = 7
-    PIN_ECHO = 11
+        GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+        GPIO.setup(PIN_ECHO, GPIO.IN)
 
-    GPIO.setup(PIN_TRIGGER, GPIO.OUT)
-    GPIO.setup(PIN_ECHO, GPIO.IN)
+        GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
-    GPIO.output(PIN_TRIGGER, GPIO.LOW)
+        print("Waiting for sensor to settle")
 
-    print("Waiting for sensor to settle")
+        time.sleep(2)
 
-    time.sleep(2)
+        print("Calculating distance")
 
-    print("Calculating distance")
+        GPIO.output(PIN_TRIGGER, GPIO.HIGH)
 
-    GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+        time.sleep(0.00001)
 
-    time.sleep(0.00001)
+        GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
-    GPIO.output(PIN_TRIGGER, GPIO.LOW)
+        while GPIO.input(PIN_ECHO) == 0:
+            pulse_start_time = time.time()
+        while GPIO.input(PIN_ECHO) == 1:
+            pulse_end_time = time.time()
 
-    while GPIO.input(PIN_ECHO) == 0:
-        pulse_start_time = time.time()
-    while GPIO.input(PIN_ECHO) == 1:
-        pulse_end_time = time.time()
-
-    pulse_duration = pulse_end_time - pulse_start_time
-    distance = round(pulse_duration * 17150, 2)
-    print("Distance:", distance, "cm")
-    if distance > 200:
-        GPIO.setwarnings(False)
-        # Select GPIO mode
-        GPIO.setmode(GPIO.BCM)
-        # Set buzzer - pin 23 as output
-        buzzer = 23
-        GPIO.setup(buzzer, GPIO.OUT)
-        # Run forever loop
-        while True:
+        pulse_duration = pulse_end_time - pulse_start_time
+        distance = round(pulse_duration * 17150, 2)
+        print("Distance:", distance, "cm")
+        if distance > 200:
+            GPIO.setwarnings(False)
+            buzzer = 23
+            GPIO.setup(buzzer, GPIO.OUT)
             GPIO.output(buzzer, GPIO.HIGH)
             print("Beep")
             sleep(0.5)  # Delay in seconds
@@ -54,8 +49,8 @@ try:
             sleep(0.5)
 
 
-finally:
-    GPIO.cleanup()
+    finally:
+        GPIO.cleanup()
 
 
 # Libraries
