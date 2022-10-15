c19b79f0ae5816221a318b723d05b2f5eaaeafc4
ErrorDecoder <ErrorDecoder@github.com>
2022-10-15 19:02:14 +0530

    Oct15

--- buzz.py
+++ buzz.py
@@ -3,21 +3,7 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 
-def buzz():
-    GPIO.setwarnings(False)
-    # Select GPIO mode
-    GPIO.setmode(GPIO.BCM)
-    # Set buzzer - pin 23 as output
-    buzzer = 23
-    GPIO.setup(buzzer, GPIO.OUT)
-    # Run forever loop
-    while True:
-        GPIO.output(buzzer, GPIO.HIGH)
-        print("Beep")
-        sleep(0.5)  # Delay in seconds
-        GPIO.output(buzzer, GPIO.LOW)
-        print("No Beep")
-        sleep(0.5)
+# def buzz():
 
 
 try:
@@ -52,8 +38,20 @@ try:
     distance = round(pulse_duration * 17150, 2)
     print("Distance:", distance, "cm")
     if distance > 200:
-
-        buzz()
+        GPIO.setwarnings(False)
+        # Select GPIO mode
+        GPIO.setmode(GPIO.BCM)
+        # Set buzzer - pin 23 as output
+        buzzer = 23
+        GPIO.setup(buzzer, GPIO.OUT)
+        # Run forever loop
+        while True:
+            GPIO.output(buzzer, GPIO.HIGH)
+            print("Beep")
+            sleep(0.5)  # Delay in seconds
+            GPIO.output(buzzer, GPIO.LOW)
+            print("No Beep")
+            sleep(0.5)
 
 
 finally:
