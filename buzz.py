39e799b0df1ae63001f2b37ff8652db73ff3a1c9
Manasi Patil <72141367+ManasiPatil2117@users.noreply.github.com>
2022-11-23 16:26:52 +0530

    Update buzz.py

--- buzz.py
+++ buzz.py
@@ -1,62 +1,49 @@
-#!/usr/bin/python
 import RPi.GPIO as GPIO
-from time import sleep
 import time
 
 
-# def buzz():
+GPIO.setmode(GPIO.BOARD)
 
-while True:
-    try:
-        GPIO.setmode(GPIO.BOARD)
+TRIG = 16
+ECHO = 18
+i=0
 
-        PIN_TRIGGER = 7
-        PIN_ECHO = 11
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
 
-        GPIO.setup(PIN_TRIGGER, GPIO.OUT)
-        GPIO.setup(PIN_ECHO, GPIO.IN)
+GPIO.output(TRIG, False)
+print "Calibrating....."
+time.sleep(2)
 
-        GPIO.output(PIN_TRIGGER, GPIO.LOW)
-
-        print("Waiting for sensor to settle")
-
-        time.sleep(2)
-
-        print("Calculating distance")
-
-        GPIO.output(PIN_TRIGGER, GPIO.HIGH)
-
-        time.sleep(0.00001)
-
-        GPIO.output(PIN_TRIGGER, GPIO.LOW)
-
-        while GPIO.input(PIN_ECHO) == 0:
-            pulse_start_time = time.time()
-        while GPIO.input(PIN_ECHO) == 1:
-            pulse_end_time = time.time()
-
-        pulse_duration = pulse_end_time - pulse_start_time
-        distance = round(pulse_duration * 17150, 2)
-        print("Distance:", distance, "cm")
-        if distance < 100:
-            GPIO.setwarnings(False)
-            buzzer = 16
-            GPIO.setup(buzzer, GPIO.OUT)
-            i = 5
-
-            while i >= 0:
-                GPIO.output(buzzer, GPIO.HIGH)
-                print("Beep")
-                sleep(0.25)  # Delay in seconds 0.25
-                GPIO.output(buzzer, GPIO.LOW)
-                print("No Beep")
-                sleep(0.25)  # sleeep
-                i = i-1  # counter
-
-    finally:
-        GPIO.cleanup()  # cleaning
+print "Place the object......"
 
 
-# Libraries
+try:
+    while True:
+       GPIO.output(TRIG, True)
+       time.sleep(0.00001)
+       GPIO.output(TRIG, False)
 
-# Disable warnings (optional)
+       while GPIO.input(ECHO)==0:
+          pulse_start = time.time()
+
+       while GPIO.input(ECHO)==1:
+          pulse_end = time.time()
+
+       pulse_duration = pulse_end - pulse_start
+
+       distance = pulse_duration * 17150
+
+       distance = round(distance+1.15, 2)
+  
+       if distance<=20 and distance>=5:
+          print "distance:",distance,"cm"
+          i=1
+          
+       if distance>20 and i==1:
+          print "place the object...."
+          i=0
+       time.sleep(2)
+
+except KeyboardInterrupt:
+     GPIO.cleanup()
