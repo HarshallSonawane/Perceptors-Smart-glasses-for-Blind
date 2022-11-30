6c590cf947b2edb86b35691161740b668e9db54a
HarshallSonawane <harshalsonawane127@gmail.com>
2022-12-01 00:46:39 +0530

    UPDATED_CAMModule

--- buzz.py
+++ buzz.py
@@ -1,62 +1,48 @@
-#!/usr/bin/python
 import RPi.GPIO as GPIO
-from time import sleep
 import time
+import os
 
 
-# def buzz():
+GPIO.setmode(GPIO.BOARD)
 
-while True:
-    try:
-        GPIO.setmode(GPIO.BOARD)
+TRIG = 16
+ECHO = 18
+i = 0
 
-        PIN_TRIGGER = 7
-        PIN_ECHO = 11
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
 
-        GPIO.setup(PIN_TRIGGER, GPIO.OUT)
-        GPIO.setup(PIN_ECHO, GPIO.IN)
+GPIO.output(TRIG, False)
+os.system("espeak 'Calibrating Distance' ")
+time.sleep(2)
 
-        GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
-        print("Waiting for sensor to settle")
-
-        time.sleep(2)
-
-        print("Calculating distance")
-
-        GPIO.output(PIN_TRIGGER, GPIO.HIGH)
-
+try:
+    while True:
+        str = 'python3 TFLite_detection_webcam.py --modeldir=Sample_TFLite_model'
+        GPIO.output(TRIG, True)
         time.sleep(0.00001)
+        GPIO.output(TRIG, False)
 
-        GPIO.output(PIN_TRIGGER, GPIO.LOW)
+        while GPIO.input(ECHO) == 0:
+            pulse_start = time.time()
 
-        while GPIO.input(PIN_ECHO) == 0:
-            pulse_start_time = time.time()
-        while GPIO.input(PIN_ECHO) == 1:
-            pulse_end_time = time.time()
+        while GPIO.input(ECHO) == 1:
+            pulse_end = time.time()
 
-        pulse_duration = pulse_end_time - pulse_start_time
-        distance = round(pulse_duration * 17150, 2)
-        print("Distance:", distance, "cm")
-        if distance < 100:
-            GPIO.setwarnings(False)
-            buzzer = 16
-            GPIO.setup(buzzer, GPIO.OUT)
-            i = 5
+        pulse_duration = pulse_end - pulse_start
 
-            while i >= 0:
-                GPIO.output(buzzer, GPIO.HIGH)
-                print("Beep")
-                sleep(0.25)  # Delay in seconds 0.25
-                GPIO.output(buzzer, GPIO.LOW)
-                print("No Beep")
-                sleep(0.25)  # sleeep
-                i = i-1  # counter
+        distance = pulse_duration * 17150
 
-    finally:
-        GPIO.cleanup()  # cleaning
+        distance = round(distance+1.15, 2)
+
+        if distance <= 150:
+            os.system("espeak 'Object Detected at ' "+distance+" ' metres ' ")
+            time.sleep(2)
+            os.system("espeak 'Launching Camera for Object Detection' ")
+            time.sleep(2)
+            os.system(str)
 
 
-# Libraries
-
-# Disable warnings (optional)
+except KeyboardInterrupt:
+    GPIO.cleanup()
